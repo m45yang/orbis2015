b529f5c2b3b763a0baf414732e8e5f68d9a929a8
m45yang <m45yang@uwaterloo.ca>
2015-10-03 23:10:39 -0400

    able to dodge bullets if given enough space

--- PlayerAI.py
+++ PlayerAI.py
@@ -4,6 +4,15 @@ from PythonClientAPI.libs.Game.MapOutOfBoundsException import *
 
 class PlayerAI:
 	def __init__(self):
+		# Dictionary that maps direction to action
+		self.direction_convert = {
+			Direction.UP: "FACE_UP",
+			Direction.DOWN: "FACE_DOWN",
+			Direction.LEFT: "FACE_LEFT",
+			Direction.RIGHT: "FACE_RIGHT",
+			True: "NO_MOVE"
+		}
+
 		# Dictionary that converts string to API commands
 		self.action_convert = {
 			"FACE_UP": Move.FACE_UP,
@@ -21,9 +30,17 @@ class PlayerAI:
 			"TELEPORT5": Move.TELEPORT_5
 		}
 		self.timer = 0
+		self.turrets = []
 
 	def get_move(self, gameboard, player, opponent):
 		# LET'S DO THIS
+
+		# first move to initialize positions of static turrets
+		if (self.timer == 0):
+			self.turrets = gameboard.turrets
+			for turret in self.turrets:
+				turret.killzone = turret_kill_zone(turret.x, turret.y, gameboard.width, gameboard.height)
+
 		return self.action_convert[find_move(self, gameboard, player, opponent)]
 
 
@@ -31,63 +48,94 @@ class PlayerAI:
 
 # finds the best possible move
 def find_move(self, gameboard, player, opponent):
-	directions = possible_moves(self, gameboard, player, opponent)
-
 	# defensive maneuvre decision tree
-	defence = defensive_action(self, gameboard, player, directions)
+	defence = defensive_action(self, gameboard, player.x, player.y)
 	if (defence):
-		return defence
+		return decide_direction(self.direction_convert, defence, player.direction)
 
-	if player.direction in directions:
-		return "SHOOT"
+	# if player.direction in directions:
+	# 	return "SHOOT"
 
-	else:
-		return "FACE_RIGHT"
+	# else:
+	# 	return "FACE_RIGHT"
 
-def defensive_action(self, gameboard, player, directions):
+
+# This shit is recursive backtracking, handle it like it's a fucking time bomb dude
+# Recursively searches through all possible defensive maneuvres and returns the first possible one
+# which will ensure survival for the next 2 turns
+def defensive_action(self, gameboard, x, y):
+	# get possible directions that are not blocked by walls
+	directions = possible_moves(self, gameboard, x, y)
+
 	bullets = gameboard.bullets
 	for bullet in bullets:
-		if (bullet.y == player.y and get_x(gameboard.width, bullet.x + 1) - player.x <= -1):
+		if (bullet.y == y and get_x(gameboard.width, bullet.x + 1) - x < 3):
 			if (bullet.direction == Direction.RIGHT):
-				directions.remove(Direction.RIGHT)
-				directions.remove(Direction.LEFT)
-				if (player.direction == Direction.RIGHT or player.direction == Direction.LEFT):
-					return "FACE_DOWN"
+				print("bullet coming from the left!")
+				up = defensive_action(self, gameboard, x, get_y(gameboard.height, y + 1))
+				if (up and Direction.UP in directions):
+					return Direction.UP
+				down = defensive_action(self, gameboard, x, get_y(gameboard.height, y - 1))
+				if (down and Direction.DOWN in directions):
+					return Direction.DOWN
 				else:
-					return "FORWARD"
-		if (bullet.y == player.y and get_x(gameboard.width, bullet.x - 1) - player.x <= 1):
+					return False
+		if (bullet.y == y and get_x(gameboard.width, bullet.x - 1) - x < -3):
 			if (bullet.direction == Direction.LEFT):
-				directions.remove(Direction.RIGHT)
-				directions.remove(Direction.LEFT)
-				if (player.direction == Direction.RIGHT or player.direction == Direction.LEFT):
-					return "FACE_UP"
+				print("bullet coming from the right!")
+				up = defensive_action(self, gameboard, x, get_y(gameboard.height, y + 1))
+				if (up and Direction.UP in directions):
+					return Direction.UP
+				down = defensive_action(self, gameboard, x, get_y(gameboard.height, y - 1))
+				if (down and Direction.DOWN in directions):
+					return Direction.DOWN
 				else:
-					return "FORWARD"
-		if (bullet.x == player.x and get_y(gameboard.height, bullet.y + 1) - player.y <= -1):
+					return False
+		if (bullet.x == x and get_y(gameboard.height, bullet.y - 1) - y < 3):
 			if (bullet.direction == Direction.UP):
-				directions.remove(Direction.UP)
-				directions.remove(Direction.DOWN)
-				if (player.direction == Direction.UP or player.direction == Direction.DOWN):
-					return "FACE_LEFT"
+				print("bullet coming from below!")
+				right = defensive_action(self, gameboard, get_x(gameboard.width, x - 1), y)
+				if (right and Direction.RIGHT in directions):
+					return Direction.RIGHT
+				left = defensive_action(self, gameboard, get_x(gameboard.width, x - 1), y)
+				if (left and Direction.LEFT in directions):
+					return Direction.LEFT
 				else:
-					return "FORWARD"
-		if (bullet.x == player.x and get_y(gameboard.height, bullet.y + 1) - player.y <= 1):
+					return False
+		if (bullet.x == x and get_y(gameboard.height, bullet.y + 1) - y < -3):
 			if (bullet.direction == Direction.DOWN):
-				directions.remove(Direction.UP)
-				directions.remove(Direction.DOWN)
-				if (player.direction == Direction.UP or player.direction == Direction.DOWN):
-					return "FACE_RIGHT"
+				print("bullet coming from above!")
+				right = defensive_action(self, gameboard, get_x(gameboard.width, x - 1), y)
+				if (right and Direction.RIGHT in directions):
+					return Direction.RIGHT
+				left = defensive_action(self, gameboard, get_x(gameboard.width, x - 1), y)
+				if (left and Direction.LEFT in directions):
+					return Direction.LEFT
 				else:
-					return "FORWARD"
+					return False
 
-		turrets = gameboard.turrets
+	return True
 
-	return False
+# returns a decision for which direction the player should turn
+def decide_direction(map, tile_to_move, current_direction):
+	if (tile_to_move == current_direction):
+		return "FORWARD"
+	else:
+		return map[tile_to_move]
 
-def decide_direction(directions):
-	return False
+# returns an array containing the kill zone of a turret when it fires
+def turret_kill_zone(x, y, width, height):
+	killzone = []
+	x = x - 4
+	y = y - 4
+	for i in range(-4, 4):
+		x = x + 1
+		y = y + 1
+		if (x != 0 and y != 0):
+			killzone.append([get_x(width, x), get_y(width, y)])
+	return killzone
 
-
+# returns the new x-coordinate after a move which compensates for wrap effect
 def get_x(width, next_move):
 	if (next_move > width - 1):
 		return 0
@@ -96,6 +144,7 @@ def get_x(width, next_move):
 	else:
 		return next_move
 
+# returns the new y-coordinate after a move which compensates for wrap effect
 def get_y(height, next_move):
 	if (next_move > height - 1):
 		return 0
@@ -104,17 +153,17 @@ def get_y(height, next_move):
 	else:
 		return next_move
 
-
 # returns array with all possible moves
-def possible_moves(self, gameboard, player, opponent):
+def possible_moves(self, gameboard, x, y):
 	moves = []
 
-	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, player.x + 1), player.y)):
+	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, x + 1), y)):
 		moves.append(Direction.RIGHT)
-	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, player.x - 1), player.y)):
+	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, x - 1), y)):
 		moves.append(Direction.LEFT)
-	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height, player.y + 1))):
+	if (not gameboard.is_wall_at_tile(x, get_y(gameboard.height, y - 1))):
 		moves.append(Direction.UP)
-	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height, player.y - 1))):
+	if (not gameboard.is_wall_at_tile(x, get_y(gameboard.height, y + 1))):
 		moves.append(Direction.DOWN)
+	print(moves)
 	return moves
