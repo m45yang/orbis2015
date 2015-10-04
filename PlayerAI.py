8845c9079201ed00fb4007b6ccb9f09759c92abb
m45yang <m45yang@uwaterloo.ca>
2015-10-03 21:16:45 -0400

    just got coffee YAY

--- PlayerAI.py
+++ PlayerAI.py
@@ -4,14 +4,8 @@ from PythonClientAPI.libs.Game.MapOutOfBoundsException import *
 
 class PlayerAI:
 	def __init__(self):
-		# Initialize any objects or variables you need here.
-		pass
-
-	def get_move(self, gameboard, player, opponent):
-		# LET'S DO THIS MOTHERFUCKERS
-
 		# Dictionary that converts string to API commands
-		action_convert = {
+		self.action_convert = {
 			"FACE_UP": Move.FACE_UP,
 			"FACE_DOWN": Move.FACE_DOWN,
 			"FACE_LEFT" : Move.FACE_LEFT,
@@ -26,50 +20,101 @@ class PlayerAI:
 			"TELEPORT4": Move.TELEPORT_4,
 			"TELEPORT5": Move.TELEPORT_5
 		}
+		self.timer = 0
 
-		return action_convert[find_move(self, gameboard, player, opponent)]
+	def get_move(self, gameboard, player, opponent):
+		# LET'S DO THIS
+		return self.action_convert[find_move(self, gameboard, player, opponent)]
 
 
-
+# HELPER METHODS #
 
 # finds the best possible move
 def find_move(self, gameboard, player, opponent):
 	directions = possible_moves(self, gameboard, player, opponent)
 
+	# defensive maneuvre decision tree
+	defence = defensive_action(self, gameboard, player, directions)
+	if (defence):
+		return defence
+
 	if player.direction in directions:
-		return "FORWARD"
+		return "SHOOT"
 
 	else:
 		return "FACE_RIGHT"
+
+def defensive_action(self, gameboard, player, directions):
+	bullets = gameboard.bullets
+	for bullet in bullets:
+		if (bullet.y == player.y and get_x(gameboard.width, bullet.x + 1) - player.x <= -1):
+			if (bullet.direction == Direction.RIGHT):
+				directions.remove(Direction.RIGHT)
+				directions.remove(Direction.LEFT)
+				if (player.direction == Direction.RIGHT or player.direction == Direction.LEFT):
+					return "FACE_DOWN"
+				else:
+					return "FORWARD"
+		if (bullet.y == player.y and get_x(gameboard.width, bullet.x - 1) - player.x <= 1):
+			if (bullet.direction == Direction.LEFT):
+				directions.remove(Direction.RIGHT)
+				directions.remove(Direction.LEFT)
+				if (player.direction == Direction.RIGHT or player.direction == Direction.LEFT):
+					return "FACE_UP"
+				else:
+					return "FORWARD"
+		if (bullet.x == player.x and get_y(gameboard.height, bullet.y + 1) - player.y <= -1):
+			if (bullet.direction == Direction.UP):
+				directions.remove(Direction.UP)
+				directions.remove(Direction.DOWN)
+				if (player.direction == Direction.UP or player.direction == Direction.DOWN):
+					return "FACE_LEFT"
+				else:
+					return "FORWARD"
+		if (bullet.x == player.x and get_y(gameboard.height, bullet.y + 1) - player.y <= 1):
+			if (bullet.direction == Direction.DOWN):
+				directions.remove(Direction.UP)
+				directions.remove(Direction.DOWN)
+				if (player.direction == Direction.UP or player.direction == Direction.DOWN):
+					return "FACE_RIGHT"
+				else:
+					return "FORWARD"
+
+		turrets = gameboard.turrets
+
+	return False
+
+def decide_direction(directions):
+	return False
+
+
+def get_x(width, next_move):
+	if (next_move > width - 1):
+		return 0
+	elif (next_move < 0):
+		return width - 1
+	else:
+		return next_move
+
+def get_y(height, next_move):
+	if (next_move > height - 1):
+		return 0
+	elif (next_move < 0):
+		return height - 1
+	else:
+		return next_move
+
 
 # returns array with all possible moves
 def possible_moves(self, gameboard, player, opponent):
 	moves = []
 
-	if (not gameboard.is_wall_at_tile(get_x(gameboard.width - 1, player.x + 1), player.y)):
+	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, player.x + 1), player.y)):
 		moves.append(Direction.RIGHT)
-	if (not gameboard.is_wall_at_tile(get_x(gameboard.width - 1, player.x - 1), player.y)):
+	if (not gameboard.is_wall_at_tile(get_x(gameboard.width, player.x - 1), player.y)):
 		moves.append(Direction.LEFT)
-	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height - 1, player.y + 1))):
+	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height, player.y + 1))):
 		moves.append(Direction.UP)
-	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height - 1, player.y - 1))):
+	if (not gameboard.is_wall_at_tile(player.x, get_y(gameboard.height, player.y - 1))):
 		moves.append(Direction.DOWN)
-
 	return moves
-
-def get_x(width, next_move):
-	print(width)
-	if (next_move > width):
-		return 0
-	elif (next_move < 0):
-		return width
-	else:
-		return next_move
-
-def get_y(height, next_move):
-	if (next_move > height):
-		return 0
-	elif (next_move < 0):
-		return height
-	else:
-		return next_move
